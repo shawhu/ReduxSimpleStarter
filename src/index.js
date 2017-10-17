132f8ee81b4f16614555acd9c495f228d03bb6f6
Harry Xiao <shawhu@gmail.com>
2017-10-17 19:06:42 +0800

    first working prototype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -16,16 +17,23 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
-        YTSearch({ key: API_KEY, term: 'iphone x' }, (videos) => this.setState({ 
+        this.videoSearch('iphone x');
+    }
+
+    videoSearch(keyword){
+        YTSearch({ key: API_KEY, term: keyword }, (videos) => this.setState({ 
             videos:videos,
             selectedVideo:videos[0] 
         }));
     }
 
     render() {
+        const videosearch = _.debounce((term)=>this.videoSearch(term),300);
+
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onTermChange={videosearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                  onVideoSelect = {selectedVideo=>this.setState({selectedVideo})}
