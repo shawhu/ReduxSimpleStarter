92b8ff9ae698086ca2cdd11e42b839c41bd94818
Harry Xiao <shawhu@gmail.com>
2017-10-17 15:59:42 +0800

    init commit remove existing stuff added a few new stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyD_I2tlvxR-l9vt_749ooQMHgUhgPeSEck';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({ key: API_KEY, term: 'iphone x' }, (videos) => this.setState({ 
+            videos:videos,
+            selectedVideo:videos[0] 
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                 onVideoSelect = {selectedVideo=>this.setState({selectedVideo})}
+                 videos={this.state.videos} 
+                />
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
